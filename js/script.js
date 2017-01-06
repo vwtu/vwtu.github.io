737394cb3c203115a0466fc3f58832cdf9283da9
vwtu <vicky.w.tu@gmail.com>
2017-01-06 12:36:58 -0800

    Update

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openMore() {
+	$(".expanded").css("display", "inline-block");
+	$(".third-section-more").css("display", "none");
+	$(".hr-more").css("display", "block");
+}
+
+function closeMore() {
+	$(".expanded").css("display", "none");
+	$(".third-section-more").css("display", "inline-block");
+	$(".hr-more").css("display", "none");
+}
